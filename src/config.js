ec500db930204b3afdb2b38927cb1b5546ef4b95
zhangjingjie <jingjie.zhang@ersoft.cn>
2023-07-24 10:17:56 +0800

    feat: 修改接口请求

--- src/config.js
+++ src/config.js
@@ -1,85 +1,27 @@
 'use strict';
 
 const alfy = require('alfy');
-const sample = require('lodash.sample');
-
-// 此 key 全采集于 github 上面 若有冒犯就先谢罪了啊哈...
-const FIXED_KEY = [
-    {
-        keyfrom: 'CoderVar',
-        key: '802458398'
-    },
-    {
-        keyfrom: 'whatMean',
-        key: '1933652137'
-    },
-    {
-        keyfrom: 'chinacache',
-        key: '1247577973'
-    },
-    {
-        keyfrom: 'huipblog',
-        key: '439918742'
-    },
-    {
-        keyfrom: 'chinacache',
-        key: '1247577973'
-    },
-    {
-        keyfrom: 'fanyi-node',
-        key: '593554388'
-    },
-    {
-        keyfrom: 'wbinglee',
-        key: '1127870837'
-    },
-    {
-        keyfrom: 'forum3',
-        key: '1268771022'
-    },
-    {
-        keyfrom: 'node-translator',
-        key: '2058911035'
-    },
-    {
-        keyfrom: 'kaiyao-robot',
-        key: '2016811247'
-    },
-    {
-        keyfrom: 'stone2083',
-        key: '1576383390'
-    },
-    {
-        keyfrom: 'myWebsite',
-        key: '423366321'
-    },
-    {
-        keyfrom: 'leecade',
-        key: '54015339'
-    },
-    {
-        keyfrom: 'github-wdict',
-        key: '619541059'
-    },
-    {
-        keyfrom: 'lanyuejin',
-        key: '2033774719'
-    },
-];
-
+const { v4:uuid } = require('uuid');
+const CryptoJS = require('crypto-js')
 module.exports = {
-    youDaoApi: 'http://fanyi.youdao.com/openapi.do',
+    youDaoApi: 'https://openapi.youdao.com/api',
     getParams: function () {
-        let selected = sample(FIXED_KEY);
+        const salt = uuid();
+        const curtime = Math.round(new Date().getTime()/1000);
+        const key = 'Gj6i5mijRxOiXyoQ5TY4RwOFI51UhipB'
+        const appKey = "3d5537600c026dc2";
+        const q = alfy.input
+        const sign = CryptoJS.SHA256(`${appKey}${q}${salt}${curtime}${key}`).toString(CryptoJS.enc.Hex)
         return {
             query: {
-                keyfrom: selected.keyfrom,
-                key: selected.key,
-                type: 'data',
-                doctype: 'json',
-                version: '1.1',
-                q: alfy.input
-
+                appKey,
+                salt,
+                curtime,
+                sign,
+                signType:'v3',
+                q,
+                from:'auto',
+                to:'auto',
             }
         }
     },
